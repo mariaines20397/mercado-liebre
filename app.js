bfa8618f7075589aa86ecb673c323e0bf395c473
mariaines20397 <69004580+mariaines20397@users.noreply.github.com>
2022-01-18 22:36:50 -0300

    Configuracion de express y creacion de la vista home

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express()
+const path=require('path')
+
+const publicPath= path.resolve(__dirname,'./public')
+
+
+app.use(express.static(publicPath))
+
+app.listen(3030,()=>{
+    console.log('Servidor corriendo');
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
